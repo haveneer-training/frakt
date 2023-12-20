5e1e2ac52007c42f807bdc0f7d62d5eb24f64b56
Mulet-J <71126090+Mulet-J@users.noreply.github.com>
2023-12-20 21:24:57 +0100

    improved naming (see rust naming convention)

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ use crate::client::client_services::ClientServices;
 
 fn main() {
     let mut client = ClientServices::new(String::from("localhost"), 8787);
-    let request = messages::fragmentrequest::FragmentRequest::new(String::from("w1"), 10);
+    let request = messages::fragment_request::FragmentRequest::new(String::from("w1"), 10);
     client.request_task(request);
 }
