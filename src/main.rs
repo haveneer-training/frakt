b1e5c77b25ca1a8934c84583e586260863249cd1
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-12-22 14:32:24 +0100

    server and client setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::net::TcpStream;
+use std::io::Write;
+
+
+fn main() -> io::Result<()> {
+    let mut stream = TcpStream::connect("127.0.0.1:8067")?;
+    let n: u32 = 258;
+
+    let s = "Hello from the client";
+    let n = s.len() as u32;
+    stream.write(&n.to_be_bytes())?;
+    stream.write(s.as_bytes())?;
+
+    Ok(())
 }
