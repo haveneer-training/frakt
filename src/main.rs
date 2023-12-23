99cfebd9c232fafbd2869e49f7f6dc3cf3e19bfb
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-12-23 12:18:11 +0100

    add args for the script

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,41 @@ use std::io;
 use std::net::TcpStream;
 use std::io::Write;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args{
+
+    /// Specify server address
+    #[arg(long, default_value = "127.0.0.1")]
+    server_address: String,
+
+    /// Specify server port 
+    #[arg(short, long, default_value = "8080")]
+    port: String,
+
+    /// Use debug version
+    #[arg(long)]
+    debug: bool,
+
+    /// User GPU or not
+    #[arg(long)]
+    gpu_use: bool
+}
+
 
 fn main() -> io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:8067")?;
-    let n: u32 = 258;
+    let args = Args::parse();
 
-    let s = "Hello from the client";
-    let n = s.len() as u32;
-    stream.write(&n.to_be_bytes())?;
-    stream.write(s.as_bytes())?;
+    println!("address: {}:{}, debug: {}, gpu: {}", args.server_address, args.port, args.debug, args.gpu_use);
+    
+    let mut stream = TcpStream::connect("127.0.0.1:8067")?;
+
+    let message = "Hello from the client";
+    let message_size = message.len() as u32;
+    stream.write(&message_size.to_be_bytes())?;
+    stream.write(message.as_bytes())?;
 
     Ok(())
 }
