5b27c7854fb833b10e8bc3352a4809d48fcb319e
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-12-25 18:11:05 +0100

    json test

--- network/src/models/fractal.rs
+++ network/src/models/fractal.rs
@@ -1,13 +1,13 @@
 use serde::{Serialize, Deserialize}; 
 use super::commmunication::Complex;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct JuliaDescriptor {
     pub c: Complex,
     pub divergence_threshold_square: f64
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub enum FreactalDescriptor {
     Julia(JuliaDescriptor)
 }
