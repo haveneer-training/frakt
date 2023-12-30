6ce6f2407e3f39fd9d1c29a7f54ffeeb314b910a
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-12-30 22:46:21 +0100

    I'm not stupid anymore

--- blue-box/src/network/communication_types.rs
+++ blue-box/src/network/communication_types.rs
@@ -1,6 +1,6 @@
-use serde::{Serialize, Deserialize}; 
+use serde::{Deserialize, Serialize};
 
-use crate::fractal::fractal_types::FreactalDescriptor;
+use crate::fractal::fractal_types::FractalDescriptor;
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct FragmentRequest {
@@ -11,10 +11,10 @@ pub struct FragmentRequest {
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct FragmentTask {
     pub id: U8Data,
-    pub fractal: FreactalDescriptor,
+    pub fractal: FractalDescriptor,
     pub max_iteration: u16,
     pub resolution: Resolution,
-    pub range: Range
+    pub range: Range,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
@@ -22,54 +22,54 @@ pub struct FragmentResult {
     pub id: U8Data,
     pub resolution: Resolution,
     pub range: Range,
-    pub pixels: PixelData
+    pub pixels: PixelData,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub enum NetworkProtocoles {
     FragmentRequest(FragmentRequest),
     FragmentTask(FragmentTask),
-    FragmentResult(FragmentResult)
+    FragmentResult(FragmentResult),
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct U8Data {
     pub offset: u32,
-    pub count: u32
+    pub count: u32,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone, Copy)]
 pub struct Complex {
     pub re: f64,
-    pub im: f64
+    pub im: f64,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct Point {
     pub x: f64,
-    pub y: f64
+    pub y: f64,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct Range {
     pub min: Point,
-    pub max: Point
+    pub max: Point,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct Resolution {
     pub nx: u16,
-    pub ny: u16
+    pub ny: u16,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct PixelData {
     pub offset: u32,
-    pub count: u32
+    pub count: u32,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct PixelIntensity {
     pub zn: f32,
-    pub count: f32
+    pub count: f32,
 }
--- blue-box/src/network/server.rs
+++ blue-box/src/network/server.rs
@@ -1,20 +1,30 @@
-use std::{net::{TcpListener, TcpStream}, io};
+use std::{
+    io,
+    net::{TcpListener, TcpStream},
+};
 
-use crate::{fractal::fractal_types::{FreactalDescriptor, JuliaDescriptor}, utils::json};
+use crate::{
+    fractal::fractal_types::{FractalDescriptor, JuliaDescriptor},
+    utils::json,
+};
 
-use super::{network::Network, communication_types::{FragmentRequest, NetworkProtocoles, FragmentTask, U8Data, Complex, Resolution, Range, Point, FragmentResult}};
-
+use super::{
+    communication_types::{
+        Complex, FragmentRequest, FragmentResult, FragmentTask, NetworkProtocoles, Point, Range,
+        Resolution, U8Data,
+    },
+    network::Network,
+};
 
 #[derive(Debug)]
 pub struct Server {
-    network: Network
+    network: Network,
 }
 
 impl Server {
-
-    pub fn new(server_address: String, port: String) -> Server{
+    pub fn new(server_address: String, port: String) -> Server {
         Server {
-            network: Network::new(server_address, port)
+            network: Network::new(server_address, port),
         }
     }
 
@@ -22,54 +32,41 @@ impl Server {
         TcpListener::bind(self.network.get_fulladdress())
     }
 
-    
-    pub fn get_work_request(stream: &mut TcpStream) -> Result<(FragmentRequest, Vec<u8>), io::Error> {
-
+    pub fn get_work_request(
+        stream: &mut TcpStream,
+    ) -> Result<(FragmentRequest, Vec<u8>), io::Error> {
         match Network::read_message(stream) {
             Ok((NetworkProtocoles::FragmentRequest(fragment), data)) => Ok((fragment, data)),
-            Ok((NetworkProtocoles::FragmentTask(_), _ )) => Err(io::Error::new(
+            Ok((NetworkProtocoles::FragmentTask(_), _)) => Err(io::Error::new(
                 io::ErrorKind::UnexpectedEof,
                 "Did not send a job request",
             )),
-            Ok((NetworkProtocoles::FragmentResult(_), _ )) => Err(io::Error::new(
+            Ok((NetworkProtocoles::FragmentResult(_), _)) => Err(io::Error::new(
                 io::ErrorKind::UnexpectedEof,
                 "Did not send a job request",
             )),
-            Err(err) => Err(err)
+            Err(err) => Err(err),
         }
     }
 
     pub fn send_work(stream: &mut TcpStream) -> Result<(), io::Error> {
-
-        let test_fragment_task = FragmentTask{
+        let test_fragment_task = FragmentTask {
             id: U8Data {
                 offset: 13,
-                count: 13
+                count: 13,
             },
             fractal: {
-                FreactalDescriptor::Julia( JuliaDescriptor {
-                    c: Complex {
-                        re: 13.2,
-                        im: 13.2
-                    },
-                    divergence_threshold_square: 13.4
+                FractalDescriptor::Julia(JuliaDescriptor {
+                    c: Complex { re: 13.2, im: 13.2 },
+                    divergence_threshold_square: 13.4,
                 })
             },
             max_iteration: 0,
-            resolution: Resolution {
-                nx: 100,
-                ny: 100
-            },
+            resolution: Resolution { nx: 100, ny: 100 },
             range: Range {
-                min: Point {
-                    x: 13.4,
-                    y: 18.4,
-                },
-                max: Point {
-                    x: 13.4,
-                    y: 18.4,
-                }
-            }
+                min: Point { x: 13.4, y: 18.4 },
+                max: Point { x: 13.4, y: 18.4 },
+            },
         };
 
         let network_test_fragment = NetworkProtocoles::FragmentTask(test_fragment_task);
@@ -81,19 +78,18 @@ impl Server {
         Ok(())
     }
 
-    pub fn get_work_done(stream: &mut TcpStream) -> Result<(FragmentResult, Vec<u8>), io::Error>{
+    pub fn get_work_done(stream: &mut TcpStream) -> Result<(FragmentResult, Vec<u8>), io::Error> {
         match Network::read_message(stream) {
             Ok((NetworkProtocoles::FragmentResult(fragment), data)) => Ok((fragment, data)),
-            Ok((NetworkProtocoles::FragmentTask(_), _ )) => Err(io::Error::new(
+            Ok((NetworkProtocoles::FragmentTask(_), _)) => Err(io::Error::new(
                 io::ErrorKind::UnexpectedEof,
                 "Did not send a job done",
             )),
-            Ok((NetworkProtocoles::FragmentRequest(_), _ )) => Err(io::Error::new(
+            Ok((NetworkProtocoles::FragmentRequest(_), _)) => Err(io::Error::new(
                 io::ErrorKind::UnexpectedEof,
                 "Did not send a job done",
             )),
-            Err(err) => Err(err)
+            Err(err) => Err(err),
         }
-
     }
 }
