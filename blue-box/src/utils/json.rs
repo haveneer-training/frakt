93d79990e7d893ff4b586a85440294f55f4e9b9d
Cristian URSU <cristian.ursu.2001@gmail.com>
2023-12-31 18:56:37 +0100

    all deleted

--- blue-box/src/utils/json.rs
+++
@@ -1,9 +0,0 @@
-use crate::network::communication_types::NetworkProtocoles;
-
-pub fn to_string(message: &NetworkProtocoles) -> Result<String, serde_json::Error> {
-    serde_json::to_string(message)
-}
-
-pub fn to_fragment(message: &str)-> Result<NetworkProtocoles, serde_json::Error>  {
-    serde_json::from_str(message)
-}
