f3b639bc2b1b39067be99ee0ac6fc7e77d6fcfda
Cristian URSU <cristian.ursu.2001@gmail.com>
2024-01-07 13:39:45 +0100

    make test pass

--- blue-box/src/utils/colors_test.rs
+++ blue-box/src/utils/colors_test.rs
@@ -1,8 +1,10 @@
 #![cfg(test)]
 
-use super::colors::get_color;
+use super::colors::color_palette;
+
 
 #[test]
 fn test_get_correct_color() {
-    get_color(10_f32);
+    let color = color_palette(0.5_f32);
+    assert_eq!(color, [0, 188, 188]);
 }
