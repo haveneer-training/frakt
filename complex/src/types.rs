44de2fa11aefd30c44b503c76174ded3dbf4f1c6
julien <julien.riviere94120@gmail.com>
2024-01-19 14:34:12 +0100

    feat : ajout des opérations mathématique sur complexe

--- complex/src/types.rs
+++ complex/src/types.rs
@@ -1,10 +1,57 @@
 pub mod calcul_type{
     use serde::{Serialize, Deserialize};
+    use std::ops::{Add, Mul, Sub, Div};
 
     // Structure de nombres complexes
-    #[derive(Debug, Serialize, PartialEq, Deserialize)]
+    #[derive(Debug, Serialize, PartialEq, Deserialize, Clone, Copy)]
     pub struct Complex {
         pub reel: f64,
         pub imaginaire: f64,
+    }
+
+    impl Add for Complex {
+        type Output = Complex;
+
+        fn add(self, rhs : Self) -> Self::Output{   //self = complex type
+            Complex {
+                reel: self.reel + rhs.reel,
+                imaginaire: self.imaginaire + rhs.imaginaire,
+            }
+        }
+    }
+
+    impl Sub for Complex {
+        type Output = Complex;
+    
+        fn sub(self, rhs: Self) -> Self::Output {
+            Complex {
+                reel: self.reel - rhs.reel,
+                imaginaire: self.imaginaire - rhs.imaginaire,
+            }
+        }
+    }
+
+    impl Mul for Complex {
+        type Output = Complex;
+    
+        fn mul(self, rhs: Self) -> Self::Output {
+            Complex {
+                reel: self.reel * rhs.reel - self.imaginaire * rhs.imaginaire,
+                imaginaire: self.reel * rhs.imaginaire + self.imaginaire * rhs.reel,
+            }
+        }
+    }
+
+    impl Div for Complex {
+        type Output = Complex;
+    
+        fn div(self, rhs: Self) -> Self::Output {
+            let denominator: f64 = rhs.reel * rhs.reel + rhs.imaginaire * rhs.imaginaire;
+    
+            Complex {
+                reel: (self.reel * rhs.reel + self.imaginaire * rhs.imaginaire) / denominator,
+                imaginaire: (self.imaginaire * rhs.reel - self.reel * rhs.imaginaire) / denominator,
+            }
+        }
     }
 }
